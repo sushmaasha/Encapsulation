dc8f714a677035ca665859caa8649ce5251b2568
sushmaasha <123888171+sushmaasha@users.noreply.github.com>
2023-07-01 12:48:53 +0530

    Add files via upload

---
+++ maintest.java
@@ -0,0 +1,12 @@
+package encapsulation;
+
+public class maintest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+test t= new test();
+t.setB(5);
+System.out.println(t.getB());
+	}
+
+}
---
+++ test.java
@@ -0,0 +1,15 @@
+package encapsulation;
+
+public class test {
+	
+	private int b;
+
+	public int getB() {
+		return b;
+	}
+
+	public void setB(int b) {
+		this.b = b;
+	}
+
+}
